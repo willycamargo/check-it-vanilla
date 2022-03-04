cbd649170ec766b66f6f5e902d05fc2381589a42
Willy Camargo <camargowilly@gmail.com>
2022-03-03 21:38:13 -0300

    Add functions to handle tasks storage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,44 @@
 const $form = document.getElementById('form-tasks')
 
+/**
+ * Funcoes de Armazenamento
+ */
+const addTaskToLocalStorage = (task) => {
+  let tasks = JSON.parse(window.localStorage.getItem('tasks'))
+
+  if (!tasks) {
+    tasks = []
+  }
+
+  tasks.push(task)
+  window.localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const removeTaskFromLocalStorage = (taskId) => {
+  const tasks = JSON.parse(window.localStorage.getItem('tasks'))
+  const taskToRemove = tasks.findIndex((task) => task.id == taskId)
+  tasks.splice(taskToRemove, 1)
+  window.localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+const updateTaskCheckedFromLocalStorage = (taskId, checked) => {
+  const tasks = JSON.parse(window.localStorage.getItem('tasks'))
+  const taskToUpdate = tasks.findIndex((task) => task.id == taskId)
+  tasks[taskToUpdate].checked = checked
+  window.localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+/**
+ * Funcoes de DOM
+ */
 const handleItemRemove = (event) => {
   const $removeButton = event.target
   const $li = $removeButton.parentElement
   const $currentList = $li.parentElement
+  const taskId = $li.dataset.id
+
   $currentList.removeChild($li)
+  removeTaskFromLocalStorage(taskId)
 }
 
 const handleCheckboxChange = (event) => {
@@ -12,18 +46,22 @@ const handleCheckboxChange = (event) => {
   const $li = $checkbox.parentElement.parentElement
   const $tasksTodoList = document.getElementById('list-todo')
   const $tasksDoneList = document.getElementById('list-done')
+  const taskId = $li.dataset.id
 
   if ($checkbox.checked) {
     $tasksDoneList.append($li)
   } else {
     $tasksTodoList.append($li)
   }
+
+  updateTaskCheckedFromLocalStorage(taskId, $checkbox.checked)
 }
 
-const addTask = (title) => {
+const addTaskToPage = ({ title, checked, id }) => {
   // Criar Item (li)
   const $li = document.createElement('li')
   $li.classList.add('tasks-section__item')
+  $li.dataset.id = id
 
   // Criar Label (label)
   const $label = document.createElement('label')
@@ -34,6 +72,7 @@ const addTask = (title) => {
   const $checkbox = document.createElement('input')
   $checkbox.classList.add('tasks-section__item__checkbox')
   $checkbox.setAttribute('type', 'checkbox')
+  $checkbox.checked = checked
   $label.append($checkbox)
 
   // Criar span (span)
@@ -48,9 +87,14 @@ const addTask = (title) => {
   $removeButton.innerText = 'X'
   $li.append($removeButton)
   
-  // Adicionar item na lista To-do
-  const $tasksTodoList = document.getElementById('list-todo')
-  $tasksTodoList.append($li)
+  // Adicionar item nas listas
+  if (checked) {
+    const $tasksTodoList = document.getElementById('list-done')
+    $tasksTodoList.append($li)
+  } else {
+    const $tasksTodoList = document.getElementById('list-todo')
+    $tasksTodoList.append($li)
+  }
 
   // Adicionar eventos
   $checkbox.addEventListener('change', handleCheckboxChange)
@@ -62,8 +106,28 @@ const handleFormSubmit = (event) => {
   const formData = new FormData($form)
   const title = formData.get('title')
 
-  addTask(title)
+  const task = {
+    title: title,
+    checked: false,
+    id: Date.now()
+  }
+  addTaskToLocalStorage(task)
+  addTaskToPage(task)
   $form.reset()
 }
 
+// Renderiza elementos do local storage
+const renderTasksFromLocalStorage = () => {
+  const tasks = JSON.parse(window.localStorage.getItem('tasks'))
+
+  if (tasks) {
+    for (let i = 0; i < tasks.length; i++) {
+      const currentTask = tasks[i]
+      addTaskToPage(currentTask)
+    }
+  }
+}
+renderTasksFromLocalStorage()
+
+// Inicialize eventos
 $form.addEventListener('submit', handleFormSubmit)
